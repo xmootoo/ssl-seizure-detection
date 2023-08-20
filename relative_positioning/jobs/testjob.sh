c54a8f0999ef6ee49bda0d864906d420ee28b6b4
Xavier Mootoo <xmootoo@gmail.com>
2023-08-20 19:22:04 -0400

    Testing for GPU support submitting testjob.sh to CC

--- relative_positioning/jobs/testjob.sh
+++ relative_positioning/jobs/testjob.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-job_name="test4_20_08_2023"
-model_logdir="$xav/ssl_epilepsy/jobs/relative_positioning/models/"
-stats_logdir="$xav/ssl_epilepsy/jobs/relative_positioning/stats/"
+job_name="test5_20_08_2023"
+# model_logdir="$xav/ssl_epilepsy/jobs/relative_positioning/models/"
+# stats_logdir="$xav/ssl_epilepsy/jobs/relative_positioning/stats/"
 
 
 sbatch <<EOT
 #!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=3
-#SBATCH --mem-per-cpu=10G
-#SBATCH --time=00:05:00
+#SBATCH --ntasks=1               # Number of cores (CPUs, threads)
+#SBATCH --gres=gpu:1             # Number of GPU(s) per node
+#SBATCH --cpus-per-task=16       # CPU cores/threads
+#SBATCH --mem-per-cpu=10G        # memory per CPU core
+#SBATCH --time=00:05:00     
 #SBATCH --job-name=$job_name
 #SBATCH --output=$xav/ssl_epilepsy/jobs/relative_positioning/tests/$job_name.out
 #SBATCH --mail-user=xmootoo@gmail.com
 #SBATCH --mail-type=ALL
 
 cd $xav/ssl_epilepsy/ssl-seizure-detection/relative_positioning/tensorflow/tests
-module load python/3.10
+module load cuda cudnn python/3.10
 source ~/tf2.12/bin/activate
 
 python testcc.py $model_logdir $stats_logdir
