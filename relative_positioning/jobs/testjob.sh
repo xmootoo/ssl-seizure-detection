84eb2fc14084ee47be05636adf56c1de580c1b24
Xavier Mootoo <xmootoo@gmail.com>
2023-08-20 17:15:02 -0400

    Edited job name out in testjob.sh

--- relative_positioning/jobs/testjob.sh
+++ relative_positioning/jobs/testjob.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+job_name="test_19_08_2023"
+
 sbatch <<EOT
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
 #SBATCH --mem-per-cpu=10G
 #SBATCH --time=00:05:00
-#SBATCH --job-name=test_19_08_2023
-#SBATCH --output=$xav/ssl_epilepsy/jobs/%j.out
+#SBATCH --job-name=$job_name
+#SBATCH --output=$xav/ssl_epilepsy/jobs/$job_name.out
 #SBATCH --mail-user=xmootoo@gmail.com
 #SBATCH --mail-type=ALL
 
