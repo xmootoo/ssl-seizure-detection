06299b23380cc577a12f581b14f3c9c0a5656362
xmootoo <xmootoo@gmail.com>
2023-09-01 03:28:17 -0400

    Fixed main script

--- relative_positioning/pytorch/pyg/pyg_main.py
+++ relative_positioning/pytorch/pyg/pyg_main.py
@@ -14,20 +14,20 @@ if __name__ == '__main__':
     lr=1e-3
     timing=True
 
-    # # Hard coded parameters
-    # data_path = r"C:\Users\xmoot\Desktop\Data\ssl-seizure-detection\patient_pseudolabeled\relative_positioning\PyG\jh101_12s_7min_PairData.pt"
-    # model_path = r"C:\Users\xmoot\Desktop\Models\PyTorch\ssl-seizure-detection\relative_positioning\jh101"
-    # stats_path = r"C:\Users\xmoot\Desktop\Models\PyTorch\ssl-seizure-detection\relative_positioning\jh101"
-    # model_name = r"\jh101_12s_7min_1epochs"
-    # num_workers = 4
+    # Hard coded parameters
+    data_path = r"C:\Users\xmoot\Desktop\Data\ssl-seizure-detection\patient_pseudolabeled\relative_positioning\PyG\jh101_12s_7min_PairData.pt"
+    model_path = r"C:\Users\xmoot\Desktop\Models\PyTorch\ssl-seizure-detection\relative_positioning\jh101"
+    stats_path = r"C:\Users\xmoot\Desktop\Models\PyTorch\ssl-seizure-detection\relative_positioning\jh101"
+    model_name = r"\jh101_12s_7min_1epochs"
+    num_workers = 4
 
-    # Bash command line parameters
-    data_path = sys.argv[1]
-    model_path = sys.argv[2]
-    stats_path = sys.argv[3]
-    model_name = sys.argv[4]
-    num_workers = int(sys.argv[5])
+    # # Bash command line parameters
+    # data_path = sys.argv[1]
+    # model_path = sys.argv[2]
+    # stats_path = sys.argv[3]
+    # model_name = sys.argv[4]
+    # num_workers = int(sys.argv[5])
 
-    train(data_path, model_path, model_name, epochs, data_size, train_ratio, batch_size, num_workers,
-            lr, timing)
+    train(data_path, model_path, stats_path, model_name, epochs, data_size, train_ratio, batch_size, num_workers,
+          lr, timing)
 
