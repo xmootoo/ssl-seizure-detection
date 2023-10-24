ac6d7b5fc6288c547c2d545b83c83a2819d502f1
xmootoo <xmootoo@gmail.com>
2023-10-24 13:14:35 -0400

    updated Cedar training script for projected run times.

--- scripts/training/cedar/train_wrapper.sh
+++ scripts/training/cedar/train_wrapper.sh
@@ -16,11 +16,11 @@ fi
 patient_id="$1"
 
 # Run ID (e.g., 1,2,3,4)
-run_id="$2"
+run_type="$2"
 
 # Training arguments
 model_ids=("supervised" "relative_positioning" "temporal_shuffling")
-times=("00:30:00" "11:59:00" "24:00:00")
+times=("03:00:00" "23:59:00" "32:00:00")
 date_and_time=$(date "+%Y-%m-%d_%H.%M.%S")
 
 # Base directory
@@ -73,6 +73,6 @@ source ~/torch2_cuda11.7/bin/activate
 
 export WANDB_API_KEY="$WANDB_API_KEY"
 
-python main.py "${data_path}" "${logdir}" "${patient_id}" "${model_id}" "${date_and_time}" "${run_id}"
+python main.py "${data_path}" "${logdir}" "${patient_id}" "${model_id}" "${date_and_time}" "${run_type}"
 EOT
 done
